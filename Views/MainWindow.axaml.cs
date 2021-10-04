8fe6d0e5bbfc41e399f96391d450d89f71e2689d
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-04 23:27:00 +0330

    Binding to a Click Event in Code

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,17 +1,26 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+// use MainWindowViewModel class
+using dotnet_gui_app.ViewModels;
 
 namespace dotnet_gui_app.Views
 {
     public partial class MainWindow : Window
     {
+        private MainWindowViewModel SpecificViewModel => DataContext as MainWindowViewModel;
+
         public MainWindow()
         {
             InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+            var clickCodeButton = this.Find<Button>("ClickCodeButton");
+            clickCodeButton.Click += delegate
+            {
+                SpecificViewModel.UpdateTextStatus("On Click Code Button");
+            };
         }
 
         private void InitializeComponent()
